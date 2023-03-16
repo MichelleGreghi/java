ae36519525ebf31b4e5fdac26cb5f20a6f52ab56
Michelle Greghi <michelle.neves23@gmail.com>
2023-03-16 12:34:48 -0300

    Aula 2 - Exercício 2

---
+++ aula_02/src/aula_02/Exercicio2.java
@@ -0,0 +1,26 @@
+package aula_02;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		int numero;
+		
+		System.out.println("Digite um número: ");
+		numero = leia.nextInt();
+		
+		if (numero < 0 && numero % 2 == 0)
+			System.out.println("O Número " + numero + " é par e negativo!");
+		else if (numero > 0 && numero % 2 == 0)
+			System.out.println("O Número " + numero + " é par e positivo!");
+		else if (numero > 0 && numero % 2 == 1)
+			System.out.println("O Número " + numero + " é ímpar e positivo!");
+		else 
+			System.out.println("O Número " + numero + " é ímpar e negativo!");
+	}
+
+}
